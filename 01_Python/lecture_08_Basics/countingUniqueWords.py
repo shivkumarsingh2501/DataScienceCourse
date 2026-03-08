52f0fee034efd6792d3c64a83a8c98751e47be5e
Shiv Kumar Singh <shivkumarsingh2501@gmail.com>
2026-03-08 14:31:10 +0530

    set lecture

---
+++ 01_Python/lecture_08_Basics/countingUniqueWords.py
@@ -0,0 +1,6 @@
+text = " in this tuturial we are discussing about sets and in this we are discussing"
+words = text.split()
+print(words)
+unique_words = set(words)
+print(unique_words)
+print(len(unique_words))
---
+++ 01_Python/lecture_08_Basics/datacleaning.py
@@ -0,0 +1,10 @@
+#conversion of list to set : mostly used for data cleaning
+
+
+number = [1,1,2,2,3,3,4,4,45,5,55,5]
+uniques_number =set(number)
+print(uniques_number)
+list_new= list(uniques_number)
+print(list_new)
+
+#this frequently used for data cleaning
